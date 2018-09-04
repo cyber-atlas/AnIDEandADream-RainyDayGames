d1b024c9922c2892cfb187d74b4c2b1684e6572b
Alex <ads97@iastate.edu>
2018-09-04 15:22:42 -0500

    I figured out the issues I was having, I needed to select a theme for the app. When I did so, it allowed me to see the elements I was placing. I then made a simple addition calculator that adds 2 numbers

--- Experiments/Alex/FirstApp/app/src/main/java/com/example/cyber_atlas/myfirstapp/MainActivity.java
+++ Experiments/Alex/FirstApp/app/src/main/java/com/example/cyber_atlas/myfirstapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.cyber_atlas.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creates a button called add, searches the resources to find the addBtn ID we created for the button we created
+        //findViewByID searches, R is our resources, id is what resources we are searching, addBtn is the button we created
+        Button addBtn  = (Button) findViewById(R.id.addBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                //Gets the text from firstNumEditText and secondNumEditText turns it into a string, then stores that integer
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+                //Set text uses strings, so we concatenate with an empty string in order to display our integer
+                resultTextView.setText(result + "");
+
+            }
+        });
     }
 }
