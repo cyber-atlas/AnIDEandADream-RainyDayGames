239ec70a7c1ec86723653301ceee811408879f5f
ads97 <ads97@iastate.edu>
2018-09-27 15:46:58 -0500

    The score button updates the textview

--- FrontEnd/RainyDayGames/app/src/main/java/com/ideandadream/rainydaygames/MainActivity.java
+++ FrontEnd/RainyDayGames/app/src/main/java/com/ideandadream/rainydaygames/MainActivity.java
@@ -2,12 +2,39 @@ package com.ideandadream.rainydaygames;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private int score = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonUpScore = (Button) findViewById(R.id.buttonUpScore);
+
+
+        final TextView textScore = (TextView) findViewById(R.id.textScore);
+
+
+        buttonUpScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                score++;
+
+                textScore.setText("Score: " + score);
+
+
+
+            }
+        });
+
     }
 }
