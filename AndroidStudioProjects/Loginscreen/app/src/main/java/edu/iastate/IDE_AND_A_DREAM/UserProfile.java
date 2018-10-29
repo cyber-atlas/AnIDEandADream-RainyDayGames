837d7aeae7c00efadca837dc365d34bfb1088967
Uma Abu <umaabu@umas-mbp.student.iastate.edu>
2018-10-29 14:49:37 -0500

    Added User profile page

---
+++ AndroidStudioProjects/Loginscreen/app/src/main/java/edu/iastate/IDE_AND_A_DREAM/UserProfile.java
@@ -0,0 +1,15 @@
+package edu.iastate.IDE_AND_A_DREAM;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.iastate.loginscreen.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
--- AndroidStudioProjects/Loginscreen/app/src/main/java/edu/iastate/IDE_AND_A_DREAM/secondpage.java
+++ AndroidStudioProjects/Loginscreen/app/src/main/java/edu/iastate/IDE_AND_A_DREAM/secondpage.java
@@ -19,6 +19,8 @@ public class secondpage extends AppCompatActivity {
         setContentView(R.layout.secondpage);
 
         Button Gamesbutton = findViewById(R.id.GamesButton);
+        Button Profilebutton = findViewById(R.id.profilebutton);
+
 
         Gamesbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,10 +30,21 @@ public class secondpage extends AppCompatActivity {
                     String value = extras.getString("userid");
                     Log.d("Message from Signup", value);
                 }
-                //startActivity(new Intent(MainActivity.this, signuppage.class));
-//                Intent GamesListIntent = new Intent(secondpage.this, GamesList.class);
-//                secondpage.this.startActivity(GamesListIntent);
                 Intent i = new Intent(secondpage.this, GamesList.class);
+                i.putExtra("userid",extras.getString("userid"));
+                secondpage.this.startActivity(i);
+            }
+        });
+
+        Profilebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    String value = extras.getString("userid");
+                    Log.d("Message from Signup", value);
+                }
+                Intent i = new Intent(secondpage.this, UserProfile.class);
                 i.putExtra("userid",extras.getString("userid"));
                 secondpage.this.startActivity(i);
             }
