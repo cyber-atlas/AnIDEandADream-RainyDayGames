6fe32e70c670ec92308235f2c3aecb7c64faa5df
Uma Abu <umaabu@Umas-MacBook-Pro.local>
2018-11-04 01:23:00 -0500

    Can upload images to Android studio but not to the website

--- AndroidStudioProjects/Loginscreen/app/src/main/java/edu/iastate/IDE_AND_A_DREAM/UserProfile/User_Profile_Main.java
+++ AndroidStudioProjects/Loginscreen/app/src/main/java/edu/iastate/IDE_AND_A_DREAM/UserProfile/User_Profile_Main.java
@@ -1,11 +1,15 @@
 package edu.iastate.IDE_AND_A_DREAM.UserProfile;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -21,12 +25,16 @@ import org.json.JSONObject;
 import edu.iastate.loginscreen.R;
 
 public class User_Profile_Main extends AppCompatActivity {
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     private TextView Email;
     private TextView Username;
     private RequestQueue mQueue;
     Button Update_Username ;
     Button Update_Password;
+
+
+    ImageView ProfilePicture;
 
 
     @Override
@@ -41,7 +49,20 @@ public class User_Profile_Main extends AppCompatActivity {
         Update_Username = findViewById(R.id.Edit_Username);
         Update_Password = findViewById(R.id.Change_pwd);
 
+        ProfilePicture = (ImageView)findViewById(R.id.Profile_Pic_view);
+
+
+
         mQueue = Volley.newRequestQueue(this);
+
+        ProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent GalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(GalleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
 
         Update_Username.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,6 +129,17 @@ public class User_Profile_Main extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null)
+        {
+            Uri selectedImage = data.getData();
+            ProfilePicture.setImageURI(selectedImage);
+        }
+
+
+    }
 
 }
