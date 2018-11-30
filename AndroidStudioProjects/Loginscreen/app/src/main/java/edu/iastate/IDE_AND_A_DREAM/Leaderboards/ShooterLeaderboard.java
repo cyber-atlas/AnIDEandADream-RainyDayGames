d81fe611d9e3ffa6e3c298e7f146aaf5ad81fe57
Merin Mundt <merinmundt@merins-mbp.student.iastate.edu>
2018-11-30 17:45:32 -0600

    names to leaders

--- AndroidStudioProjects/Loginscreen/app/src/main/java/edu/iastate/IDE_AND_A_DREAM/Leaderboards/ShooterLeaderboard.java
+++ AndroidStudioProjects/Loginscreen/app/src/main/java/edu/iastate/IDE_AND_A_DREAM/Leaderboards/ShooterLeaderboard.java
@@ -18,6 +18,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import edu.iastate.loginscreen.R;
 
@@ -27,6 +28,9 @@ public class ShooterLeaderboard extends AppCompatActivity{
     private RequestQueue mQueue;
 
     ArrayList<Integer> scorelist = new ArrayList<>();
+    ArrayList<Integer> userids = new ArrayList<>();
+    HashMap<Integer, String> names = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +38,42 @@ public class ShooterLeaderboard extends AppCompatActivity{
         simpleList = findViewById(R.id.simpleListView);
         Log.d("Tag", "in java");
         final String url = "http://proj309-vc-04.misc.iastate.edu:8080/scores?game=1";
+        final String userurl = "http://proj309-vc-04.misc.iastate.edu:8080/users";
         mQueue = Volley.newRequestQueue(this);
 
-        JsonArrayRequest request= new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, userurl, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try{
+
+                    for (int i = 0; i < response.length(); i++){
+                        JSONObject usernames;
+                        usernames = response.getJSONObject(i);
+
+                        int id = usernames.getInt("id");
+                        String name = usernames.getString("username");
+
+                        names.put(id, name);
+
+
+
+                    }
+                }catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(req);
+        mQueue.start();
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 try {
@@ -47,8 +84,10 @@ public class ShooterLeaderboard extends AppCompatActivity{
                         user = response.getJSONObject(i);
 
                         int score = user.getInt("score");
+                        int name = user.getInt("userid");
                         Log.d("Tag", user.toString());
                         scorelist.add(score);
+                        userids.add(name);
                     }
 
                 } catch (JSONException e1) {
@@ -57,12 +96,14 @@ public class ShooterLeaderboard extends AppCompatActivity{
 
 
 
-                ArrayList<Integer> shortList = new ArrayList<>();
+                ArrayList<String> shortList = new ArrayList<>();
+
                 for(int i = 0; i < 10; i++){
-                    shortList.add(scorelist.get(i));
+                    shortList.add(scorelist.get(i) + " - "+ names.get(userids.get(i)));
+
                 }
 
-                ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(ShooterLeaderboard.this, R.layout.activity_text_view, shortList);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(ShooterLeaderboard.this, R.layout.activity_text_view, shortList);
                 ListView listView = findViewById(R.id.simpleListView);
                 listView.setAdapter(adapter);
 
@@ -76,6 +117,7 @@ public class ShooterLeaderboard extends AppCompatActivity{
         });
 
         mQueue.add(request);
+
 
 
 
